bbf8c24ed4ea2ae96ce06a17e5be6dca31b9848c
yash chawda <yashchawda2685@gmail.com>
2026-01-05 10:39:59 +0530

    first commit of v1 python

---
+++ 1_practice.py
@@ -0,0 +1,34 @@
+print("Hello world",end=" ")
+print("Hello world world yash ji")
+print("I am yash","45",
+      "indore")
+
+print("""This is a para
+of the checking
+        
+        the """)
+
+#camel case
+myVariableName = "yash"
+
+#pascal case
+MyVariableName = "yash yash"
+
+#snake case
+my_variable_name = "test"
+
+x,y,z = 1,2,3
+print(x,y,z)
+
+x = y = z = 5
+print(x, y, z)
+
+ls = ["A","B","C"]
+a,b,c = ls
+print(a,b,c)
+
+a1 = 5
+a2 = 5
+print(int(a1/a2))
+
+
